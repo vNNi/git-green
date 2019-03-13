df54b0dcdcf78b294367b2e78765f4fddb1eb00c
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-03-13 16:13:22 -0300

    entry point of bot

---
+++ index.js
@@ -0,0 +1,11 @@
+const repository = require('./repository');
+
+const prompt = require('prompt-sync')();
+
+const token = prompt('Tell me ur token access: ');
+const repoName = prompt('Tell me ur repository name: ');
+const descript = prompt('Tell me ur repository description: ');
+const private = JSON.parse(prompt('Is it private? true or false '));
+
+
+repository.create(token,repoName,descript,private);
