de23aceac135368df223fbd34e9a39065f24abac
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-03-13 18:27:12 -0300

    add details

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const repository = require('./repository');
 
 const prompt = require('prompt-sync')();
 
+//github access token, its can be taken from the settings on github.
 const token = prompt('Tell me ur token access: ');
 const repoName = prompt('Tell me ur repository name: ');
 const descript = prompt('Tell me ur repository description: ');
@@ -9,7 +10,7 @@ const private = JSON.parse(prompt('Is it private? true or false '));
 
 
 repository.create(token,repoName,descript,private).then((response)=>{
-    console.log('\x1b[32m', response);
+    console.log('\x1b[32m', response); // green color log
 }).catch((error)=>{
-    console.log('\x1b[31m', error);
+    console.log('\x1b[31m', error); // red color log
 });
